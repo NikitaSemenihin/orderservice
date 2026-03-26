b2c1d372cd777766768f7dfa71cb97f7a929874b
Nikita <semenihin.rink@gmail.com>
2026-03-27 00:23:41 +0300

    ORDER-0: PR comments fix
    add TRUSTED_PACKAGES and DeadLetterPublishingRecoverer implementation

--- src/main/java/com/innowise/orderservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/innowise/orderservice/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.innowise.orderservice.config;
 
 import com.innowise.orderservice.exception.OrderNotFoundException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.DeserializationException;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -32,7 +34,7 @@ public class KafkaConsumerConfig {
         props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
 
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.innowise.orderservice.event");
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.innowise.orderservice.event.PaymentCreatedEvent");
         props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
 
@@ -40,15 +42,17 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory
-    ) {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
+    public DefaultErrorHandler kafkaErrorHandler(KafkaTemplate<Object, Object> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+                kafkaTemplate,
+                (record, e) -> new TopicPartition(record.topic() + ".DLT", record.partition())
+        );
 
-        factory.setConsumerFactory(consumerFactory);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                recoverer,
+                new FixedBackOff(1000L, 2L)
+        );
 
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new FixedBackOff(1000L, 2L));
         errorHandler.addNotRetryableExceptions(
                 OrderNotFoundException.class,
                 DeserializationException.class,
@@ -56,7 +60,20 @@ public class KafkaConsumerConfig {
                 IllegalArgumentException.class
         );
 
-        factory.setCommonErrorHandler(errorHandler);
+        return errorHandler;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            DefaultErrorHandler kafkaErrorHandler
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(kafkaErrorHandler);
+
         return factory;
     }
 }
